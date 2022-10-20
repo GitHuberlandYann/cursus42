79eecad576a6dc2c6af7e2c26fe866575ff00b17
Yann Huberland <yhuberla@shi-c2-p3.s19.be>
2022-10-20 11:40:24 +0200

    Modified atoi to make it clean af

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: yhuberla <yhuberla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 15:00:46 by yhuberla          #+#    #+#             */
-/*   Updated: 2022/10/14 09:46:06 by yhuberla         ###   ########.fr       */
+/*   Updated: 2022/10/20 11:38:55 by yhuberla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 
 int	ft_atoi(const char *str)
 {
-	size_t	index;
-	size_t	res;
+	int		index;
 	int		sign;
-	int		strlen;
+	long	res;
+	long	save;
 
 	index = 0;
 	sign = 1;
 	res = 0;
-	strlen = 0;
 	while (str[index] == ' ' || (str[index] >= '\t' && str[index] <= '\r'))
 		index ++;
 	if (str[index] == '+' || str[index] == '-')
@@ -30,13 +29,12 @@ int	ft_atoi(const char *str)
 		if (str[index ++] == '-')
 			sign *= -1;
 	}
-	while (str[index] == '0')
-		index ++;
-	while (str[index] >= '0' && str[index] <= '9' && ++strlen < 23)
+	while (str[index] >= '0' && str[index] <= '9')
+	{
+		save = res;
 		res = res * 10 + str[index ++] - '0';
-	if ((res > 9223372036854775807 || strlen >= 20) && sign == 1)
-		return (-1);
-	else if ((res - 1 > 9223372036854775807 || strlen >= 20) && sign == -1)
-		return (0);
+		if (res < save)
+			return (-(sign == 1));
+	}
 	return (res * sign);
 }
