101c8b21ec3b6945c58a331c34bb700c966e8e7d
Yann Huberland <yhuberla@shi-c2-p3.s19.be>
2022-10-27 16:02:56 +0200

    [PS] Args management

---
+++ PushSwap/Libft/libft.h
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/10 17:41:03 by yhuberla          #+#    #+#             */
+/*   Updated: 2022/10/22 13:33:07 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdarg.h>
+
+int	    ft_atoi(const char *str);
+int     ft_arraylen(char **array);
+size_t	ft_strlen(const char *str);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char **str);
+char	*ft_strcpy_from_nl(char **str);
+char	*ft_strcpy_until_nl(char *str);
+char	**ft_split(char const *s, char c);
+
+void		ft_printf(const char *str, ...);
+int		ft_abs(int nb);
+int		ft_nbrlen(int nb);
+char	ft_get_type(char c);
+void	ft_putchar(unsigned char c);
+void	ft_putchars(unsigned char c, int size);
+void	ft_putstr(char *str);
+void	ft_putnbr(int nb);
+void	ft_putunbr(unsigned int nb);
+void	ft_puthexa(unsigned int nb, int upper);
+void	ft_putaddresshexa(void *ptr);
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
+
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+
+#endif
---
+++ PushSwap/SRCS/main.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yhuberla <yhuberla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/27 15:37:22 by yhuberla          #+#    #+#             */
+/*   Updated: 2022/10/27 16:01:30 by yhuberla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int *ft_free_return(int *tab)
+{
+	free(tab);
+	return (0);
+}
+
+static int	ft_check_arg(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index])
+	{
+		if (str[index] < '0' || str[index] > '9')
+			return (0);
+		++index;
+	}
+	return (1);
+}
+
+static int	*ft_check_args(int ac, char **av)
+{
+	int	index;
+	int	*res;
+
+	res = malloc(sizeof(*res) * (ac - 1));
+	if (!res)
+		return (0);
+	index = 1;
+	while (index < ac)
+	{
+		if (!ft_check_arg(av[index]))
+			return (ft_free_return(res));
+		res[index - 1] = ft_atoi(av[index]);
+		++index;
+	}
+	return (res);
+}
+
+int	main(int ac, char **av)
+{
+	int	*taba;
+	int	*tabb;
+
+	if (ac > 1)
+	{
+		taba = ft_check_args(ac, av);
+		if (taba)
+		{
+			ft_putstr("enough arguments.\n");
+			tabb = malloc(sizeof(*tabb) * (ac - 1));
+			if (tabb)
+			{
+				ft_compute(taba, tabb);
+				free(tabb);
+			}
+			free(taba);
+		}
+		else
+			ft_putstr("Error\n");
+	}
+	return (0);
+}
