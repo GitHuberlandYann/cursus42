5c42841f02e6e66732628ad1bb0b60f37577aa61
Yann Huberland <yhuberla@shi-c2-p3.s19.be>
2022-11-17 16:05:45 +0100

    [pipex] did something I guess

--- pipex/SRCS/execve.c
+++ pipex/SRCS/execve.c
@@ -6,30 +6,31 @@
 /*   By: yhuberla <yhuberla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 10:48:09 by yhuberla          #+#    #+#             */
-/*   Updated: 2022/11/17 12:43:52 by yhuberla         ###   ########.fr       */
+/*   Updated: 2022/11/17 13:41:27 by yhuberla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	ft_exec_forkcat(t_parent child_p, char *path, char **envp)
+static void	ft_exec_forkcat(t_parent *child_p, char *path, char **envp)
 {
 	char		*args[3];
 
-	args[0] = "pipex"; //anything will do, just needs something,
+	args[0] = "/bin/cat"; //anything will do, just needs something,
 						//convention = name of current executable
 	args[1] = path;
 	args[2] = 0;
-	ft_fork(&child_p.c_pid);
-	if (!child_p.c_pid) //CHILD OF CHILD
+	ft_fork(&child_p->c_pid);
+	if (!child_p->c_pid) //CHILD OF CHILD
 	{
-		dup2(child_p.pipefd[1], 1); //output of cat is input of next cmd
-		execve("/bin/cat", args, envp);
+		ft_putstr_fd("cat\n", 1);
+		dup2(child_p->pipefd[1], 1); //output of cat is input of next cmd
+		execve(args[0], args, envp);
 		ft_perror(strerror(errno));
 	}
 	else
 	{
-		ft_wait_child(child_p);
+		ft_wait_child(*child_p);
 		ft_putstr_fd("parent in forkcat\n", 1);
 	}
 }
@@ -52,12 +53,17 @@ void	ft_exec_main_child(t_parent p, char **av, char **envp)
 		ft_perror(strerror(ENOMEM));
 	// ft_putstr_fd(cmd, 1);
 	ft_pipe(child_p.pipefd);
-	ft_exec_forkcat(child_p, av[1], envp);
-	//ft_putstr_fd(cmd, 1);
+	ft_exec_forkcat(&child_p, av[1], envp);
 	dup2(child_p.pipefd[0], 0);
 	dup2(p.pipefd[1], 1); //output of cmd is input of next cmd
 	execve(cmd, cmds, envp);
-	ft_perror(strerror(errno));
+	free(cmd);
+	cmd = ft_strjoin("/usr/bin/", cmds[0]);
+	if (!cmd)
+		ft_perror(strerror(ENOMEM));
+	execve(cmd, cmds, envp);
+	ft_putstr_fd(cmd, 1);
+	ft_perror(cmd);
 }
 
 // void	ft_exec_first_command(char **av, char **envp, char **gnl)
