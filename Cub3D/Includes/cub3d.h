8ebd9efac731d9b05a380b5ae97d5609df69f08c
Yann Huberland <yhuberla@shi-c2-p3.s19.be>
2023-01-31 15:58:18 +0100

    [cub3D] init parsing

--- Cub3D/Includes/cub3d.h
+++ Cub3D/Includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: yhuberla <yhuberla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 12:56:52 by yhuberla          #+#    #+#             */
-/*   Updated: 2023/01/31 13:14:19 by yhuberla         ###   ########.fr       */
+/*   Updated: 2023/01/31 15:20:55 by yhuberla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,45 @@
 
 // Includes
 # include <unistd.h>
+# include <fcntl.h>
 
 // Macros
+# define MSG_ARGS "Usage :\n./cub3D maps/<insert map name>.cub\n"
+# define MSG_MAP " -> invalid map\n"
+# define MSG_PREFIX "Map should start with 'maps/'"
+# define MSG_SUFFIX "Map should end with '.cub'"
+# define MSG_OPEN "Open failed"
+
 
 // Structures
-typedef struct s_map {
+typedef struct s_player {
+	float	x;
+	float	y;
+	float	z;
+	float	direction;
+}				t_player;
 
+typedef struct s_wall	t_wall;
+struct s_wall {
+	int		x;
+	int		y;
+	t_wall	*next;
+};
+
+typedef struct s_map {
+	t_player		*player;
+	t_wall			*walls;
+	char			*north_texture;
+	char			*south_texture;
+	char			*west_texture;
+	char			*east_texture
+	unsigned int	floor_color;
+	unsigned int	ceilling_color;
 }				t_map;
 
-
-
-
+typedef struct s_settings {
+	int	FOV;
+}				t_settings;
 
 // Outputs
 int		output_error(char *msg);
--- Cub3D/Sources/Parsing/map.c
+++ Cub3D/Sources/Parsing/map.c
@@ -6,15 +6,63 @@
 /*   By: yhuberla <yhuberla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 13:12:44 by yhuberla          #+#    #+#             */
-/*   Updated: 2023/01/31 13:13:20 by yhuberla         ###   ########.fr       */
+/*   Updated: 2023/01/31 15:57:58 by yhuberla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+static int	check_filename(char *file)
+{
+	int	index;
+
+	if (ft_strnstr("maps/", file, 5))
+		return (output_error(MSG_PREFIX));
+	index = ft_strlen(file) - 5;
+	if (ft_strncmp(".cub", file, 5))
+		return (output_error(MSG_SUFFIX));
+}
+
+static int	read_first_lines(t_map *map, int fd)
+{
+	char	*line;
+
+	while (line)
+	{
+		if (!ft_strncmp("NO ", line, 3))
+		else if (!ft_strncmp("SO ", line, 3))
+		else if (!ft_strncmp("WE ", line, 3))
+		else if (!ft_strncmp("EA ", line, 3))
+
+		else if (!ft_strncmp("F ", line, 2))
+		else if (!ft_strncmp("C ", line, 2))
+		else if (line[0] == '\n')
+		else
+			break ;
+		free(line);
+	}
+}
+
 int	load_map(t_map *map, char *file)
 {
-	(void)map;
-	(void)file;
+	int	fd;
+
+	if (check_filename(file))
+		return (1);
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		return (ft_perror(file));
+	map->north_texture = 0;
+	map->south_texture = 0;
+	map->west_texture = 0;
+	map->east_texture = 0;
+	map->floor_color = 0;
+	map->ceilling_color = 0;
+	map->walls = 0;
+	if (read_first_lines(map, fd))
+		return (1);
+	if (read_map(map, fd))
+		return (1);
+	close(fd);
 	return (0);
 }
