8f78d84349e2febf2047d947562f978beca364d7
GitHuberlandYann <49492714+GitHuberlandYann@users.noreply.github.com>
2023-03-10 12:17:00 +0100

    [cub3d] a bit of norming a day keeps the joy away

--- Cub3D/Sources/RayCasting/ray_posting.c
+++ Cub3D/Sources/RayCasting/ray_posting.c
@@ -12,143 +12,82 @@
 
 #include "cub3d.h"
 
-void	solve_singular(t_ray *ray, t_vert *abc, double delta, double m, double p, t_vert *trpt, t_vert *center, double radius)
+static void	solve_singular(t_ray *ray, t_solve *solve, t_vert *trpt,
+	t_post *post)
 {
 	t_vert	intersection;
 	double	dist;
 
-	intersection.x = -abc->y + sqrt(delta) / (2 * abc->x);
-	intersection.y = m * intersection.x + p;
+	intersection.x = -solve->b + sqrt(solve->delta) / (2 * solve->a);
+	intersection.y = solve->m * intersection.x + solve->p;
 	dist = get_dist(*trpt, intersection);
 	if (dist < ray->dist)
 	{
-		set_point(&ray->ray.pt2, intersection.x + center->x, intersection.y + center->y, 0);
+		set_point(&ray->ray.pt2, intersection.x + post->center.x,
+			intersection.y + post->center.y, 0);
 		ray->dist = dist;
 		ray->hit = CIRCLE;
-		ray->u = ft_fmod(acos(intersection.x / radius), 1);
+		ray->u = ft_fmod(acos(intersection.x / post->radius), 1);
 	}
 }
 
-void	solve_dual(t_ray *ray, t_vert *abc, double delta, double m, double p, t_vert *trpt, t_vert *center, double radius)
+static void	set_newshortest(t_ray *ray, t_vert *intersection, t_post *post,
+	double dist)
+{
+	set_point(&ray->ray.pt2, intersection->x + post->center.x,
+		intersection->y + post->center.y, 0);
+	ray->dist = dist;
+	ray->hit = CIRCLE;
+	ray->u = ft_fmod(acos(intersection->x / post->radius), 1);
+}
+
+static void	solve_dual(t_ray *ray, t_solve *solve, t_vert *trpt, t_post *post)
 {
 	t_vert	intersection;
 	t_vert	intersectionbis;
 	double	dist;
 	double	distbis;
 
-	intersection.x = (-abc->y + sqrt(delta)) / (2 * abc->x);
-	intersection.y = m * intersection.x + p;
+	intersection.x = (-solve->b + sqrt(solve->delta)) / (2 * solve->a);
+	intersection.y = solve->m * intersection.x + solve->p;
 	intersection.z = 0;
 	dist = get_dist(*trpt, intersection);
-	intersectionbis.x = (-abc->y - sqrt(delta)) / (2 * abc->x);
-	intersectionbis.y = m * intersectionbis.x + p;
+	intersectionbis.x = (-solve->b - sqrt(solve->delta)) / (2 * solve->a);
+	intersectionbis.y = solve->m * intersectionbis.x + solve->p;
 	intersectionbis.z = 0;
 	distbis = get_dist(*trpt, intersectionbis);
-	if ((distbis < dist && (-cos(ray->angle) / (trpt->x - intersectionbis.x) > 0 || sin(ray->angle) / (trpt->y - intersectionbis.y) > 0))
-		|| !(-cos(ray->angle) / (trpt->x - intersection.x) > 0 || sin(ray->angle) / (trpt->y - intersection.y) > 0))
+	if ((distbis < dist && (-cos(ray->angle) / (trpt->x - intersectionbis.x) > 0
+				|| sin(ray->angle) / (trpt->y - intersectionbis.y) > 0))
+		|| !(-cos(ray->angle) / (trpt->x - intersection.x) > 0
+			|| sin(ray->angle) / (trpt->y - intersection.y) > 0))
 	{
 		dist = distbis;
 		intersection = intersectionbis;
 	}
-	if (dist < ray->dist && (-cos(ray->angle) / (trpt->x - intersection.x) > 0 || sin(ray->angle) / (trpt->y - intersection.y) > 0))
-	{
-		set_point(&ray->ray.pt2, intersection.x + center->x, intersection.y + center->y, 0);
-		ray->dist = dist;
-		ray->hit = CIRCLE;
-		ray->u = ft_fmod(acos(intersection.x / radius), 1);
-	}
+	if (dist < ray->dist && (-cos(ray->angle) / (trpt->x - intersection.x) > 0
+			|| sin(ray->angle) / (trpt->y - intersection.y) > 0))
+		set_newshortest(ray, &intersection, post, dist);
 }
 
 void	ray_posting(t_post *posts, t_ray *ray)
 {
-	double	m;
-	double	p;
-	t_vert	abc;
-	double	delta;
+	t_solve	solve;
 	t_vert	trpt;
 
 	while (posts)
 	{
-		set_point(&trpt, ray->ray.pt1.x - posts->center.x, ray->ray.pt1.y - posts->center.y, 0);
-		m = -tan(ray->angle);
-		p = trpt.y - m * trpt.x;
-		abc.x = 1 + m * m;
-		abc.y = 2 * p * m;
-		abc.z = p * p - posts->squared.z;
-		delta = abc.y * abc.y - 4 * abc.x * abc.z;
-		if (!delta)
-			solve_singular(ray, &abc, delta, m, p, &trpt, &posts->center, posts->radius);
-		else if (delta > 0)
-			solve_dual(ray, &abc, delta, m, p, &trpt, &posts->center, posts->radius);
+		set_point(&trpt, ray->ray.pt1.x - posts->center.x,
+			ray->ray.pt1.y - posts->center.y, 0);
+		solve.m = -tan(ray->angle);
+		solve.p = trpt.y - solve.m * trpt.x;
+		solve.a = 1 + solve.m * solve.m;
+		solve.b = 2 * solve.p * solve.m;
+		solve.c = solve.p * solve.p - posts->squared.z;
+		solve.delta = solve.b * solve.b - 4 * solve.a * solve.c;
+		if (!solve.delta)
+			solve_singular(ray, &solve, &trpt, posts);
+		else if (solve.delta > 0)
+			solve_dual(ray, &solve, &trpt, posts);
 		posts = posts->next;
 	}
 }
-
-// void	solve_singular(t_ray *ray, t_vert *abc, double delta, double m, double p)
-// {
-// 	t_vert	intersection;
-// 	double	dist;
-
-// 	intersection.x = -abc->y + sqrt(delta) / (2 * abc->x);
-// 	intersection.y = m * intersection.x + p;
-// 	dist = get_dist(ray->ray.pt1, intersection);
-// 	if (dist < ray->dist)
-// 	{
-// 		ray->ray.pt2 = intersection;
-// 		ray->dist = dist;
-// 		ray->hit = CIRCLE;
-// 		//todo ray->u ?? I guess acos or asin of inter.x-center.x
-// 	}
-// }
-
-// void	solve_dual(t_ray *ray, t_vert *abc, double delta, double m, double p)
-// {
-// 	t_vert	intersection;
-// 	t_vert	intersectionbis;
-// 	double	dist;
-// 	double	distbis;
-
-// 	intersection.x = (-abc->y + sqrt(delta)) / (2 * abc->x);
-// 	intersection.y = m * intersection.x + p;
-// 	intersection.z = 0;
-// 	dist = get_dist(ray->ray.pt1, intersection);
-// 	intersectionbis.x = (-abc->y - sqrt(delta)) / (2 * abc->x);
-// 	intersectionbis.y = m * intersectionbis.x + p;
-// 	intersectionbis.z = 0;
-// 	distbis = get_dist(ray->ray.pt1, intersectionbis);
-// 	if (distbis < dist && (-cos(ray->angle)/(ray->ray.pt1.x - intersectionbis.x) > 0 || sin(ray->angle) / (ray->ray.pt1.y - intersectionbis.y) > 0))
-// 	{
-// 		dist = distbis;
-// 		intersection = intersectionbis;
-// 	}
-// 	if (dist < ray->dist && (-cos(ray->angle)/(ray->ray.pt1.x - intersection.x) > 0 || sin(ray->angle) / (ray->ray.pt1.y - intersection.y) > 0))
-// 	{
-// 		ray->ray.pt2 = intersection;
-// 		ray->dist = dist;
-// 		ray->hit = CIRCLE;
-// 		//todo ray->u ?? I guess acos or asin of inter.x-center.x
-// 	}
-// }
-
-// void	ray_posting(t_post *posts, t_ray *ray)
-// {
-// 	double	m;
-// 	double	p;
-// 	t_vert	abc;
-// 	double	delta;
-
-// 	m = -tan(ray->angle);
-// 	p = ray->ray.pt1.y - m * ray->ray.pt1.x;
-// 	while (posts)
-// 	{
-// 		abc.x = 1 + m * m;
-// 		abc.y = 2 * (p * m - posts->center.y * m - posts->center.x);
-// 		abc.z = posts->squared.x + p * p - 2 * p * posts->squared.y + posts->squared.y - posts->squared.z;
-// 		delta = abc.y * abc.y - 4 * abc.x * abc.z;
-// 		if (!delta)
-// 			solve_singular(ray, &abc, delta, m, p);
-// 		else if (delta > 0)
-// 			solve_dual(ray, &abc, delta, m, p);
-// 		posts = posts->next;
-// 	}
-// }
