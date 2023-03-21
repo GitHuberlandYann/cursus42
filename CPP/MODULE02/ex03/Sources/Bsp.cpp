f28e5cddfaf8d0e5f6956a246e177a4cabe9eaf6
Yann Huberland <yhuberla@shi-c2-p3.s19.be>
2023-03-21 17:52:58 +0100

    [cpp] Bsp using Fixed points

--- CPP/MODULE02/ex03/Sources/Bsp.cpp
+++ CPP/MODULE02/ex03/Sources/Bsp.cpp
@@ -12,24 +12,25 @@
 
 #include "Point.hpp"
 
-static float	ft_abs( float nb ) {
-	if (nb < 0)
-		return (-nb);
+static Fixed	ft_abs( Fixed nb ) {
+	if (nb < 0) {
+		return (nb * Fixed(-1));
+	}
 	return (nb);
 }
 
-static float	area( Point const a, Point const b, Point const c ) {
+static Fixed	area( Point const a, Point const b, Point const c ) {
 	Fixed const	div(2);
-	return (ft_abs(((a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY()) + c.getX() * (a.getY() - b.getY())) / div).toFloat()));
+	return (ft_abs((a.getX() * (b.getY() - c.getY()) + b.getX() * (c.getY() - a.getY()) + c.getX() * (a.getY() - b.getY())) / div));
 }
 
 bool			bsp( Point const a, Point const b, Point const c, Point const point) {
-	float	total = area(a, b, c);
-	float	sub_1 = area(a, b, point);
-	float	sub_2 = area(b, c, point);
-	float	sub_3 = area(c, a, point);
+	Fixed	total( area(a, b, c) );
+	Fixed	sub_1( area(a, b, point) );
+	Fixed	sub_2( area(b, c, point) );
+	Fixed	sub_3( area(c, a, point) );
 	// std::cout << total << " == " << sub_1 << " + " << sub_2 << " + " << sub_3 << std::endl;
-	if (!sub_1 || !sub_2 || !sub_3)
+	if (sub_1 == 0 || sub_2 == 0 || sub_3 == 0)
 		return (0);
 	return (total == (sub_1 + sub_2 + sub_3));
 }
